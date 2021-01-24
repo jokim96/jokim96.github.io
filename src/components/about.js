1271f861f9d818e12f0ce27c29928fb6b2cd13e8
joshkim <jjskim96@gmail.com>
2021-01-24 17:20:37 -0500

    update

--- src/components/about.js
+++ src/components/about.js
@@ -15,16 +15,16 @@ function About(props) {
         <NavLink to="/film" className="about-link">  Filmmaker &gt;</NavLink>
         </div>
         <div className="about-me">
-          <h2>Visual Learner. Visual Creator.</h2>
+       
           
-          <p>I am a junior studying Computer Science and Modern Culture and Media from South Korea and Singapore and currently studying at Brown University in the US. <br></br>I like to create anything that lies in the intersection between tech, storytelling, and visuality - be it filmmaking, design or coding. </p>
+          <p>Final Year student at Brown University studying Computer Science and Modern Culture and Media. <br></br>I enjoy working in the intersection between tech and visual storytelling. </p>
           <br></br>
           <p>joshua_kim@brown.edu</p>
         </div>
       </div>
 
       <div className="footer">
-        <p>© Joshua Kim 2020</p>
+        <p>© Joshua Kim 2021</p>
       </div>
     </Page>
   )
